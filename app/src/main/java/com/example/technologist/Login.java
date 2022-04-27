bf263097ec4aa64a081d6073cfa85ca08af1fd79
Eduard Dudin <edudin997@gmail.com>
2022-04-27 19:50:09 +0700

    Release 1.0

---
+++ app/src/main/java/com/example/technologist/Login.java
@@ -0,0 +1,129 @@
+package com.example.technologist;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Login extends AppCompatActivity {
+
+    // Переменные
+    ProgressDialog progressDialog;
+    TextInputEditText username, password;
+    Button login;
+    String url_logged, url_login, str_username, str_password, str_token;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    Boolean isLogin;
+    // Переменные
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Регистрируем token приложения
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            System.out.println("Fetching FCM registration token failed");
+                            return;
+                        }
+                        // Get new FCM registration token
+                        String token = task.getResult();
+                        // Log and toast
+                        System.out.println(token);
+                        str_token = token;
+                    }
+                });
+        // Регистрируем token приложения
+
+        // Присвоение значений переменным
+        url_logged = "https://eddfit.ru/soldering/logged.php";
+        url_login = "https://eddfit.ru/soldering/login.php";
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        sharedPreferences = getSharedPreferences("SHARED_PREF", MODE_PRIVATE);
+        isLogin = sharedPreferences.getBoolean("LOGGED", false);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Загрузка...");
+        // Присвоение значений переменным
+
+        // Проверка сессии
+        if (isLogin) {
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+        // Проверка сессии
+
+        // Клик на кнопку "Войти"
+        login.setOnClickListener(v -> {
+            progressDialog.show();
+            str_username = Objects.requireNonNull(username.getText()).toString().trim();
+            str_password = Objects.requireNonNull(password.getText()).toString().trim();
+            editor = sharedPreferences.edit();
+            StringRequest logged = new StringRequest(Request.Method.POST, url_logged, response -> {
+                if (response.equalsIgnoreCase("Успешно")){
+                    isLogin = true;
+                    editor.putString("USERNAME", str_username);
+                    editor.putBoolean("LOGGED", isLogin);
+                    editor.apply();
+                    session();
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                } else Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
+            }, error -> Toast.makeText(this, "Ошибка соединения", Toast.LENGTH_SHORT).show()){
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String,String> params = new HashMap<>();
+                    params.put("username", str_username);
+                    params.put("password", str_password);
+                    params.put("token", str_token);
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(logged);
+        });
+        // Клик на кнопку "Войти"
+    }
+
+    // Метод сессии пользователя
+    private void session() {
+        StringRequest login = new StringRequest(Request.Method.POST, url_login, response -> {
+                progressDialog.dismiss();
+                Toast.makeText(this, response, Toast.LENGTH_SHORT).show(); },
+                error -> Toast.makeText(this, "Ошибка соединения", Toast.LENGTH_SHORT).show()){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String,String> params = new HashMap<>();
+                params.put("username", str_username);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(login);
+    }
+    // Метод сессии пользователя
+}
---
+++ app/src/main/java/com/example/technologist/MainActivity.java
@@ -0,0 +1,153 @@
+package com.example.technologist;
+
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Переменные
+    MyFirebaseMessagingService database;
+    ProgressDialog progressDialog;
+    TextView username_logged, answer, title_message, body_message;
+    TextInputEditText material, periodicity, power;
+    Button logout, to_send;
+    String url_logout, url_parameters, username, str_material, str_periodicity, str_power, test;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    Toolbar toolbar;
+    // Переменные
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerReceiver(receiver,new IntentFilter("com.push.message.received"));
+
+        // Присвоение значений переменным
+        setSupportActionBar(toolbar);
+        url_logout = "https://eddfit.ru/soldering/logout.php";
+        url_parameters = "https://eddfit.ru/soldering/parameters.php";
+        username_logged = findViewById(R.id.username_logged);
+        answer = findViewById(R.id.answer);
+        title_message = findViewById(R.id.title_message);
+        body_message = findViewById(R.id.body_message);
+        logout = findViewById(R.id.logout);
+        material = findViewById(R.id.material);
+        periodicity = findViewById(R.id.periodicity);
+        power = findViewById(R.id.power);
+        to_send = findViewById(R.id.to_send);
+        preferences = getSharedPreferences("SHARED_PREF", MODE_PRIVATE);
+        username = preferences.getString("USERNAME", "");
+        username_logged.setText(username);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Загрузка...");
+        // Присвоение значений переменным
+
+        // Клик на кнопку "Выход"
+        logout.setOnClickListener(view -> {
+            progressDialog.show();
+            session();
+        });
+        // Клик на кнопку "Выход"
+
+        // Клик на кнопку "Отправить"
+        to_send.setOnClickListener(view -> {
+            progressDialog.show();
+            str_material = Objects.requireNonNull(material.getText()).toString().trim();
+            str_periodicity = Objects.requireNonNull(periodicity.getText()).toString().trim();
+            str_power = Objects.requireNonNull(power.getText()).toString().trim();
+            StringRequest parameters = new StringRequest(Request.Method.POST, url_parameters, response -> {
+                if (response.equalsIgnoreCase("Успешно")) {
+                    progressDialog.dismiss();
+                    Toast.makeText(this, response, Toast.LENGTH_SHORT).show(); } },
+                    error -> Toast.makeText(this, "Ошибка соединения", Toast.LENGTH_SHORT).show()){
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String,String> params = new HashMap<>();
+                    params.put("username", username);
+                    params.put("material", str_material);
+                    params.put("periodicity", str_periodicity);
+                    params.put("power", str_power);
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(parameters);
+            answer.setText("Ожидаем ответ оператора");
+            answer.setTextColor(Color.parseColor("#FF0000"));
+            answer.setVisibility(answer.VISIBLE);
+            title_message.setVisibility(title_message.INVISIBLE);
+            body_message.setVisibility(body_message.INVISIBLE);
+        });
+    }
+    // Клик на кнопку "Отправить"
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // intent will holding data show the data here
+            String answer_message = intent.getStringExtra("answer_message");
+            String title = intent.getStringExtra("title");
+            String body = intent.getStringExtra("body");
+            answer.setText(answer_message);
+            answer.setTextColor(Color.parseColor("#008000"));
+            title_message.setText(title);
+            title_message.setVisibility(title_message.VISIBLE);
+            body_message.setText(body);
+            body_message.setVisibility(body_message.VISIBLE);
+        }
+    };
+
+    // Метод сессии пользователя
+    private void session() {
+        StringRequest logout = new StringRequest(Request.Method.POST, url_logout, response -> {
+            if (response.equalsIgnoreCase("Успешно")) {
+                progressDialog.dismiss();
+                editor = preferences.edit();
+                editor.clear();
+                editor.apply();
+                Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish(); } },
+                error -> Toast.makeText(this, "Ошибка соединения", Toast.LENGTH_SHORT).show()){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String,String> params = new HashMap<>();
+                params.put("username", username);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(logout);
+    }
+    // Метод сессии пользователя
+}
